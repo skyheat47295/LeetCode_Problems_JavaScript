34ba8ff263e028e3e73d505b6a8b1dd9ecae58ea
skyheat47295 <70936076+skyheat47295@users.noreply.github.com>
2021-08-02 16:29:22 -0700

    8.2.21

--- Reverse Linked List.js
+++ Reverse Linked List.js
@@ -54,7 +54,6 @@ class ListNode {
     let curPre = head.next;
     current.next = null;
     while (curPre) {
-        // save the next node
         let tmp = curPre.next;
         curPre.next = current;
         current = curPre;
